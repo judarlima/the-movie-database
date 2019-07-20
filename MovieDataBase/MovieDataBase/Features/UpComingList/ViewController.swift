f5dee3f86bae95e0a2a0c9ce763b8cf094527bce
Judar Lima <judar.rslima@gmail.com>
2019-07-20 15:01:46 -0300

    Test build to fail at bitrise.

--- MovieDataBase/MovieDataBase/Features/UpComingList/ViewController.swift
+++ MovieDataBase/MovieDataBase/Features/UpComingList/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .blue
+        fatalError()
         // Do any additional setup after loading the view.
     }
 
