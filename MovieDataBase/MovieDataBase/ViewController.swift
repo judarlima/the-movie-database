a5f384e4fed1997204a9ef30ba9539158afd2d29
Judar Lima <judar.rslima@gmail.com>
2019-07-20 14:31:11 -0300

    Creates the base project.

---
+++ MovieDataBase/MovieDataBase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDataBase
+//
+//  Created by Judar Lima on 20/07/19.
+//  Copyright © 2019 Judar Lima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
