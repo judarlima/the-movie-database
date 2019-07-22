18841a3c09bdcb03f020708e44bed04ec48cdfe8
Judar Lima <judar.rslima@gmail.com>
2019-07-22 01:13:42 -0300

    Adds method to display movie details.

--- MovieDataBase/MovieDataBase/Features/UpcomingMovies/View/MovieTableViewCell.swift
+++ MovieDataBase/MovieDataBase/Features/UpcomingMovies/View/MovieTableViewCell.swift
@@ -9,39 +9,40 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
-    
+
     @IBOutlet private weak var containerView: UIView!
     @IBOutlet private weak var movieImageView: UIImageView!
     @IBOutlet private weak var titleLabel: UILabel!
     @IBOutlet private weak var genreLabel: UILabel!
     @IBOutlet private weak var releaseLabel: UILabel!
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         setup()
     }
-    
+
     override func prepareForReuse() {
         self.titleLabel.text = nil
         self.genreLabel.text = nil
         self.releaseLabel.text = nil
         self.movieImageView.image = UIImage(named: "image_placeholder")
     }
-    
+
     private func setup() {
         containerView.layer.cornerRadius = 8.0
         containerView.clipsToBounds = true
     }
-    
+
     func bind(viewModel: MovieViewModel) {
         self.titleLabel.text = viewModel.title
         self.genreLabel.text = viewModel.genre
         self.releaseLabel.text = viewModel.releaseDate
         self.movieImageView.loadImage(url: viewModel.poster)
     }
-    
+
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        super.setSelected(false, animated: true)
     }
     
 }
--- MovieDataBase/MovieDataBase/Features/UpcomingMovies/View/UpcomingMoviesViewController.swift
+++ MovieDataBase/MovieDataBase/Features/UpcomingMovies/View/UpcomingMoviesViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 
 protocol UpcomingMoviesDisplayProtocol: AnyObject {
-    func displayMovies(viewModel: [MovieViewModel])
+    func displayMovies(viewModels: [MovieViewModel])
+    func showDetails(viewModel: MovieViewModel)
 }
 
 class UpcomingMoviesViewController: UIViewController {
     private let cellIdentifier = String(describing: MovieTableViewCell.self)
+    @IBOutlet private weak var searchBar: UISearchBar!
     @IBOutlet private weak var loadingView: UIView!
     @IBOutlet private weak var tableView: UITableView!
     private let interactor: UpcomingMoviesInteractorProtocol
+    private let coordinator: UpcomingCoordinatorProtocol
     private var movies: [MovieViewModel] = []
 
-    init(interactor: UpcomingMoviesInteractorProtocol) {
+    init(interactor: UpcomingMoviesInteractorProtocol, coordinator: UpcomingCoordinatorProtocol) {
         self.interactor = interactor
+        self.coordinator = coordinator
         super.init(nibName: String(describing: UpcomingMoviesViewController.self),
                    bundle: Bundle.main)
     }
@@ -36,6 +40,7 @@ class UpcomingMoviesViewController: UIViewController {
     }
 
     private func setup() {
+        searchBar.delegate = self
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(UINib(nibName: cellIdentifier, bundle: Bundle.main),
@@ -44,15 +49,22 @@ class UpcomingMoviesViewController: UIViewController {
 }
 
 extension UpcomingMoviesViewController: UpcomingMoviesDisplayProtocol {
-    func displayMovies(viewModel: [MovieViewModel]) {
-        self.movies = viewModel
+    func displayMovies(viewModels: [MovieViewModel]) {
+        self.movies = viewModels
         tableView.reloadData()
         loadingView.isHidden = true 
+    }
+
+    func showDetails(viewModel: MovieViewModel) {
+        coordinator.movieDetails(viewModel: viewModel)
     }
 }
 
 extension UpcomingMoviesViewController: UITableViewDelegate {
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewModel = movies[indexPath.row]
+        interactor.seeDetails(viewModel: viewModel)
+    }
 }
 
 extension UpcomingMoviesViewController: UITableViewDataSource {
@@ -75,5 +87,23 @@ extension UpcomingMoviesViewController: UITableViewDataSource {
         if indexPath.row == lastMovieAtList  {
             interactor.nextMoviesPage()
         }
+    }
+}
+
+extension UpcomingMoviesViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.text = ""
+        self.searchBar.resignFirstResponder()
+        let indexPath = IndexPath(item: 0, section: 0)
+        self.tableView.scrollToRow(at: indexPath,
+                                   at: UITableView.ScrollPosition.middle, animated: true)
+        interactor.cancelSearch()
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.searchBar.resignFirstResponder()
+        guard let textToSearch = searchBar.text else { return }
+        let trimmedString = textToSearch.trimmingCharacters(in: .whitespacesAndNewlines)
+        interactor.searchMovies(text: trimmedString)
     }
 }
